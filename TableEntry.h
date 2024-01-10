a47c4d9a09f71fc246d815a18f5ff1f4c18df8d9
Adrián García Masip <upvadrian@gmail.com>
2024-01-10 17:05:49 +0100

    Añadido Makefile y código de test de la clase TableEntry

--- TableEntry.h
+++ TableEntry.h
@@ -6,43 +6,41 @@
 
 template <typename V> 
 class TableEntry {
-    public:
-	    std::string key;
-	    V value;
-	    TableEntry(std::string key, V value){
-	    	this-> key = key;
-		this-> value = value;
-	    }
+	public:
+		std::string key;
+		V value;
 
-	    TableEntry(std::string key){
-	    	this-> key = key;
-	    }
+		TableEntry(std::string key, V value){
+			this->key = key;
+			this->value = value;
+		}
+		TableEntry(std::string key){
+			this->key = key;
+		}
 
-	    TableEntry(){
+		TableEntry(){
+			key = "";
+		}
 
-	    }
+		friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			return te1.key == te2.key;
+		}
+		friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			return te1.key != te2.key;
+		}
+		friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
+			out << "('" << te.key << "' => " << te.value << ")";
+			return out;
+		}
 
-	    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
-	    	return te1.key = te2.key;
-	    }
-
-	    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
-	    	return te1.key != te2.key;
-	    }
-
-	    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
-	    	out << "( '" << te.key<<" ' => " << te.value << " )";
-		return out;
-	    }
-
-	    friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2){
-	    	return te1.key < te2.key;
-	    }
-
-	    friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2){
-	    	return te2.key > te2.key;
-	    }
-
+		friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			return te1.key < te2.key;
+		}
+		
+		friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			return te1.key > te2.key;
+		}
+		     
 };
 
 #endif
